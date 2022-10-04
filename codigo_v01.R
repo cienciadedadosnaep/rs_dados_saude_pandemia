1fc37cc36f92a0574770f45e18c0910cc2a9ebe0
assuerolopo <assuerolopo@gmail.com>
2022-10-03 23:37:40 -0300

    Treinando povoamento do subtema pandemia. Alguns ajustes precisam ser realizados.

---
+++ codigo_v01.R
@@ -0,0 +1,154 @@
+######################################################
+# 1) Carregar bibliotecas
+install.packages("tidyverse")
+install.packages("dplyr")
+insstall.package("rjson")
+install.packages("RJSONIO")
+library(tidyverse)
+library(magrittr)
+library(dplyr)
+library(readr)
+library(rjson)
+library(RJSONIO)
+
+# # Library para importar dados SQL
+# library(DBI)
+# library(RMySQL)
+# library(pool)
+# library(sqldf)
+# library(RMariaDB)
+# 
+# # Carragamento de banco de dados
+# 
+# # Settings
+# db_user <-'admin'
+# db_password <-'password'
+# db_name <-'cdnaep'
+# #db_table <- 'your_data_table'
+# db_host <-'127.0.0.1' # for local access
+# db_port <-3306
+# 
+# # 3. Read data from db
+# # drv=RMariaDB::MariaDB(),
+# mydb <-  dbConnect(drv =RMariaDB::MariaDB(),user =db_user, 
+#                    password = db_password ,
+#                    dbname = 'cdnaep', host = db_host, port = db_port)
+# 
+# dbListTables(mydb)
+# 
+# s <- paste0("SELECT * from", " consumo_agua")
+# rs<-NULL
+# rs <- dbSendQuery(mydb, s)
+# 
+# dados<- NULL
+# dados <-  dbFetch(rs, n = -1)
+# dados
+# #dbHasCompleted(rs)
+# #dbClearResult(rs)
+
+
+library(readr)
+library(readr)
+all_data_v02 <- read_csv("data/obitos_covid.csv", 
+                                      col_types = cols(DATA = col_date(format = "%m/%d/%Y")))
+#Qual é o número de mortes por COVID de em Salvador (acumulativo)?
+dados <- all_data_v02 %>% select(DATA, -CONFIRMADO, OBITO) %>%  arrange(OBITO)
+names(dados) <- c('mês','Total')
+dados$mês <-  format(dados$mês, '%m/%Y')
+
+
+
+
+##  Perguntas e titulos 
+T_ST_P_No_SAUDE <- read_csv("data/TEMA_SUBTEMA_P_No - SAUDE.csv")
+#dados <- dados %>% add_column(valor = 'valor', .after = 'trimestre')
+
+#dados %<>% gather(key = classe,
+#                  value = valor,-trimestre,-Homens) 
+
+
+#dados %<>% select(-id)
+# Temas Subtemas Perguntas
+
+
+
+## Arquivo de saida 
+
+SAIDA_POVOAMENTO <- T_ST_P_No_SAUDE %>% 
+  select(TEMA,SUBTEMA,PERGUNTA,NOME_ARQUIVO_JS)
+SAIDA_POVOAMENTO <- as.data.frame(SAIDA_POVOAMENTO)
+
+#classes <- NULL
+#classes <- levels(as.factor(dados$classe))
+
+# Cores secundarias paleta pantone -
+corsec_recossa_azul <- c('#a094e1','#dc6f6c','#62acd1','#8bc6d2',
+                         '#d62839','#20cfef','#fe4641','#175676')
+# Cor 1 - Roxo; Cor 2, 5, 7 - Vermelho; Cor 3, 4, 6, 8 - Azul
+
+simbolo_linhas <- c('emptyCircle','emptyTriangle','emptySquare',
+                    'emptyDiamond','emptyRoundRect')
+
+#for ( i in 1:length(classes)) {
+
+objeto_0 <- dados %>%
+  #filter(classe %in% c(classes[1])) %>%
+  select(mês,Total) %>% #filter(ano<2019) %>%
+  arrange(Total) %>%
+  mutate(mês = as.character(mês)) %>% list()               
+
+exportJson0 <- toJSON(objeto_0)
+
+
+titulo<-T_ST_P_No_SAUDE$TITULO[2]# posicao da pergunta
+subtexto<-"Fonte: Secretaria Municipal de Saúde de Salvador"
+link <- T_ST_P_No_SAUDE$LINK[2]
+
+data_axis <- paste('["',gsub(' ','","',
+                             paste(paste(as.vector(objeto_0[[1]]$mês)),
+                                   collapse = ' ')),'"]',sep = '')
+
+
+data_serie <- paste('[',gsub(' ',',',
+                             paste(paste(as.vector(objeto_0[[1]]$Total)),
+                                   collapse = ' ')),']',sep = '')
+
+#Colocar o nome da coluna depois de "objeto_0[[1]]$"
+
+texto<-paste('{"title":{"text":"',titulo,
+             '","subtext":"',subtexto,
+             '","sublink":"',link,'"},',
+             '"tooltip":{"trigger":"axis"},',
+             '"toolbox":{"left":"center","orient":"horizontal","itemSize":20,"top":20,"show":true,',
+             '"feature":{"dataZoom":{"yAxisIndex":"none"},',
+             '"dataView":{"readOnly":false},',
+             '"restore":{},"saveAsImage":{}}},"legend":{"show":true,"top":"bottom"},"xAxis":{"type":"category",',
+             '"data":',data_axis,'},',
+             '"yAxis":{"type":"value","axisLabel":{"formatter":"{value}"}},',
+             '"series":[{"name":"',nomes[2],'","data":',data_serie,',',
+             '"type":"bar","color":"',corsec_recossa_azul[5],'","showBackground":true,',
+             '"backgroundStyle":{"color":"rgba(180, 180, 180, 0.2)"},"symbol":"',simbolo_linhas[4],
+             '","symbolSize":10,"itemStyle":{"borderRadius":10,"borderColor":"',corsec_recossa_azul[5],'","borderWidth":2}}',
+             ']}',sep='')
+
+#SAIDA_POVOAMENTO$CODIGO[i] <- texto  
+texto<-noquote(texto)
+
+
+write(exportJson0,file = paste('data/',gsub('.csv','',T_ST_P_No_SAUDE$NOME_ARQUIVO_JS[2]),
+                               '.json',sep =''))
+write(texto,file = paste('data/',T_ST_P_No_SAUDE$NOME_ARQUIVO_JS[2],
+                         sep =''))
+
+#}
+
+# Arquivo dedicado a rotina de atualizacao global. 
+
+write_csv2(SAIDA_POVOAMENTO,file ='data/POVOAMENTO.csv',quote='all',escape='none')
+#quote="needed")#,escape='none')
+
+
+objeto_autm <- SAIDA_POVOAMENTO %>% list()
+exportJson_aut <- toJSON(objeto_autm)
+
+#write(exportJson_aut,file = paste('data/povoamento.json'))
