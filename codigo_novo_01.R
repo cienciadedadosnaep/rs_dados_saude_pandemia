ceda64e1ed4f611c41bdfe3730c5d05df0be7eb1
Luiza Moura <luizamoura90@gmail.com>
2023-06-14 13:58:07 -0300

    novos dados de 2022 para casos com status confirmado

--- codigo_novo_01.R
+++ codigo_novo_01.R
@@ -13,23 +13,40 @@ library(RJSONIO)
 
 #dados de 2020
 dados_gerais <- read_csv("data/dadosGerais2020.csv")
+#dados de 2021
+dados_gerais2021 <- read_csv("data/dadosGerais2021.csv")
 
 #view(dados_gerais)
-glimpse(dados_gerais)
+glimpse(dados_gerais2021)
 
 #Todos os casos estão com status confirmado
-dados_status <- table(dados_gerais$STATUS) 
+dados_status <- table(dados_gerais$STATUS)
+dados_status21 <- table(dados_gerais2021$STATUS)
 
 #separar data da confirmação
+#2020
 dados_classificacao <- dados_gerais[,c("DATA_DA_NOTIFICACAO", "STATUS")]
 dados_classificacao$mes <- month(dados_classificacao$DATA_DA_NOTIFICACAO)
+#2021
+dados_classificacao2021 <- dados_gerais2021[,c("DATA_DA_NOTIFICACAO", "STATUS")]
+dados_classificacao2021$mes <- month(dados_classificacao2021$DATA_DA_NOTIFICACAO)
 #view(dados_classificacao)
 
 #Quantidade por mês
+#2020
 dados <- table(dados_classificacao$mes)
 dados <- as.data.frame(dados)
 names(dados) <- c("Mês", "Número de casos confirmados")
 nomes <- names(dados)
+#2021
+dados2 <- table(dados_classificacao2021$mes)
+dados2 <- as.data.frame(dados2)
+names(dados2) <- c("Mês", "Número de casos confirmados")
+#nomes <- names(dados)
+
+
+#colocar os nomes dos meses e anos
+#2020
 glimpse(dados)
 dados <- dados |>
   mutate(Mês = as.character(Mês))
@@ -57,13 +74,42 @@ dados <- dados |>
   mutate(Mês = recode(Mês, "11" = "Novembro/2020"))
 dados <- dados |>
   mutate(Mês = recode(Mês, "12" = "Dezembro/2020"))
+#2021
+dados2 <- dados2 |>
+  mutate(Mês = as.character(Mês))
+dados2 <- dados2 |>
+  mutate(Mês = recode(Mês, "1" = "Janeiro/2021"))
+dados2 <- dados2 |>
+  mutate(Mês = recode(Mês, "2" = "Fevereiro/2021"))
+dados2 <- dados2 |>
+  mutate(Mês = recode(Mês, "3" = "Março/2021"))
+dados2 <- dados2 |>
+  mutate(Mês = recode(Mês, "4" = "Abril/2021"))
+dados2 <- dados2 |>
+  mutate(Mês = recode(Mês, "5" = "Maio/2021"))
+dados2 <- dados2 |>
+  mutate(Mês = recode(Mês, "6" = "Junho/2021"))
+dados2 <- dados2 |>
+  mutate(Mês = recode(Mês, "7" = "Julho/2021"))
+dados2 <- dados2 |>
+  mutate(Mês = recode(Mês, "8" = "Agosto/2021"))
+dados2 <- dados2 |>
+  mutate(Mês = recode(Mês, "9" = "Setembro/2021"))
+dados2 <- dados2 |>
+  mutate(Mês = recode(Mês, "10" = "Outubro/2021"))
+dados2 <- dados2 |>
+  mutate(Mês = recode(Mês, "11" = "Novembro/2021"))
+dados2 <- dados2 |>
+  mutate(Mês = recode(Mês, "12" = "Dezembro/2021"))
 
+#arredondar para visualização
 dados %<>% mutate(`Número de casos confirmados` = round(`Número de casos confirmados`/1000,2))
+dados2 %<>% mutate(`Número de casos confirmados` = round(`Número de casos confirmados`/1000,2))
 
+#juntar dados de 2020 e 2021
+dados <- rbind(dados, dados2)
 
-
-
-
+#view(dados)
 
 
 glimpse(dados)
@@ -116,7 +162,7 @@ data_serie <- paste('[',gsub(' ',',',
 texto<-paste('{"title":{"text":"',titulo,
              '","subtext":"',subtexto,
              '","sublink":"',link,'"},',
-             '"tooltip":{"trigger":"item","responsive":true,"position":"top","formatter":"{c} mil"},',
+             '"tooltip":{"trigger":"item","responsive":true,"position":"top","formatter":"{b}: {c} mil"},',
              '"toolbox":{"left":"center","orient":"horizontal","itemSize":20,"top":20,"show":true,',
              '"feature":{"dataZoom":{"yAxisIndex":"none"},',
              '"dataView":{"readOnly":false},',
@@ -125,7 +171,7 @@ texto<-paste('{"title":{"text":"',titulo,
              '"yAxis":{"type":"value","axisLabel":{"formatter":"{value} mil"}},',
              '"graphic":[{"type":"text","left":"center","top":"bottom","z":100,"style":{"fill":"gray","text":"Obs: Ponto é separador decimal", "font":"8px sans-srif","fontSize":12}}],',
              '"series":[{"name":"',nomes[2],'","data":',data_serie,',',
-             '"type":"bar","color":"',corsec_recossa_azul[1],'","showBackground":true,',
+             '"type":"line","color":"',corsec_recossa_azul[1],'","showBackground":true,',
              '"backgroundStyle":{"color":"rgba(180, 180, 180, 0.2)"},"symbol":"',simbolo_linhas[1],
              '","symbolSize":10,"itemStyle":{"borderRadius":10,"borderColor":"',corsec_recossa_azul[1],'","borderWidth":2}}',
              ']}',sep='')
