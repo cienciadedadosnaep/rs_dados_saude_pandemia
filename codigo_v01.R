5fca236bbcaf2e71b54c9216caf031bdff0b6fd2
Luiza Moura <luizamoura90@gmail.com>
2023-06-21 16:38:57 -0300

    mudança em todo o código da pergunta e atualização dos dados

--- codigo_v01.R
+++ codigo_v01.R
@@ -1,75 +1,57 @@
 ######################################################
 # 1) Carregar bibliotecas
-install.packages("tidyverse")
-install.packages("dplyr")
-insstall.package("rjson")
-install.packages("RJSONIO")
+#install.packages("tidyverse")
+#install.packages("dplyr")
+#insstall.package("rjson")
+#install.packages("RJSONIO")
 library(tidyverse)
 library(magrittr)
 library(dplyr)
 library(readr)
 library(rjson)
 library(RJSONIO)
-
-# # Library para importar dados SQL
-# library(DBI)
-# library(RMySQL)
-# library(pool)
-# library(sqldf)
-# library(RMariaDB)
-# 
-# # Carragamento de banco de dados
-# 
-# # Settings
-# db_user <-'admin'
-# db_password <-'password'
-# db_name <-'cdnaep'
-# #db_table <- 'your_data_table'
-# db_host <-'127.0.0.1' # for local access
-# db_port <-3306
-# 
-# # 3. Read data from db
-# # drv=RMariaDB::MariaDB(),
-# mydb <-  dbConnect(drv =RMariaDB::MariaDB(),user =db_user, 
-#                    password = db_password ,
-#                    dbname = 'cdnaep', host = db_host, port = db_port)
-# 
-# dbListTables(mydb)
-# 
-# s <- paste0("SELECT * from", " consumo_agua")
-# rs<-NULL
-# rs <- dbSendQuery(mydb, s)
-# 
-# dados<- NULL
-# dados <-  dbFetch(rs, n = -1)
-# dados
-# #dbHasCompleted(rs)
-# #dbClearResult(rs)
-
-
 library(readr)
-library(readr)
-all_data_v02 <- read_csv("data/obitos_covid.csv", 
-                                      col_types = cols(DATA = col_date(format = "%m/%d/%Y")))
+library(lubridate)
+
+
+
+
+all_data_v01 <- read_csv("data/confirmadoObito.csv", 
+                                      col_types = cols(DATA = col_date(format = "%d/%m/%Y")))
+
+
 #Qual é o número de mortes por COVID de em Salvador (acumulativo)?
-dados <- all_data_v02 %>% select(DATA, -CONFIRMADO, OBITO) %>%  arrange(OBITO)
-names(dados) <- c('mês','Total')
-dados$mês <-  format(dados$mês, '%m/%Y')
+dados <- all_data_v01 %>% select(DATA, -CONFIRMADO, OBITO) %>%  arrange(OBITO)
+
+#criar coluna com o último dia de cada mês
+dados$last_day <- ceiling_date(ymd(dados$DATA), 'month') - days(1)
+glimpse(dados)
+
+#df com os valores apenas do último dia de cada mês
+dados2 <- subset(dados, DATA == last_day)
+
+#escrever por extenso o mes e ano (as vígulas servem para separar um dado do outro posteriormente no data_axis)
+dados2$ano_mes_escrito <- paste(format(dados2$DATA, "%B"), format(dados2$DATA, "%Y"),",")
+
+#arredondamento
+dados2 %<>% mutate(OBITO = round(OBITO/1000,2))
+
+#tirar vígula do último dado
+dados2 |>
+  mutate(ano_mes_escrito= as.character(ano_mes_escrito))
+dados2 <- dados2 |>
+  mutate(ano_mes_escrito = recode(ano_mes_escrito, "maio 2023 ," = "maio 2023"))
+glimpse(dados2)
+
+dados2 <- dados2[,c("OBITO", "ano_mes_escrito")]
+names(dados2) <- c("Número de mortes", "Data")
+nomes <- names(dados2)
+
+#view(dados)
+view(dados2)
 
 
-
-
-##  Perguntas e titulos 
-T_ST_P_No_SAUDE <- read_csv("data/TEMA_SUBTEMA_P_No - SAUDE.csv")
-#dados <- dados %>% add_column(valor = 'valor', .after = 'trimestre')
-
-#dados %<>% gather(key = classe,
-#                  value = valor,-trimestre,-Homens) 
-
-
-#dados %<>% select(-id)
-# Temas Subtemas Perguntas
-
+T_ST_P_No_SAUDE <- read_csv("data/TEMA_SUBTEMA_P_No -  SAUDE.csv")
 
 
 ## Arquivo de saida 
@@ -78,8 +60,7 @@ SAIDA_POVOAMENTO <- T_ST_P_No_SAUDE %>%
   select(TEMA,SUBTEMA,PERGUNTA,NOME_ARQUIVO_JS)
 SAIDA_POVOAMENTO <- as.data.frame(SAIDA_POVOAMENTO)
 
-#classes <- NULL
-#classes <- levels(as.factor(dados$classe))
+
 
 # Cores secundarias paleta pantone -
 corsec_recossa_azul <- c('#a094e1','#dc6f6c','#62acd1','#8bc6d2',
@@ -89,49 +70,50 @@ corsec_recossa_azul <- c('#a094e1','#dc6f6c','#62acd1','#8bc6d2',
 simbolo_linhas <- c('emptyCircle','emptyTriangle','emptySquare',
                     'emptyDiamond','emptyRoundRect')
 
-#for ( i in 1:length(classes)) {
 
-objeto_0 <- dados %>%
+
+objeto_0 <- dados2 %>%
   #filter(classe %in% c(classes[1])) %>%
-  select(mês,Total) %>% #filter(ano<2019) %>%
-  arrange(Total) %>%
-  mutate(mês = as.character(mês)) %>% list()               
+  select(Data,`Número de mortes`) %>% #filter(ano<2019) %>%
+  #arrange(trimestre) %>%
+  mutate(Data= as.character(Data)) %>% list()               
 
 exportJson0 <- toJSON(objeto_0)
 
 
-titulo<-T_ST_P_No_SAUDE$TITULO[2]# posicao da pergunta
-subtexto<-"Fonte: Secretaria Municipal de Saúde de Salvador"
+titulo<-T_ST_P_No_SAUDE$TITULO[2]
+subtexto<-"Fonte: Saúde Salvador"
 link <- T_ST_P_No_SAUDE$LINK[2]
 
-data_axis <- paste('["',gsub(' ','","',
-                             paste(paste(as.vector(objeto_0[[1]]$mês)),
+data_axis <- paste('["',gsub(',','","',
+                             paste(paste(as.vector(objeto_0[[1]]$Data)),
                                    collapse = ' ')),'"]',sep = '')
 
 
+
 data_serie <- paste('[',gsub(' ',',',
-                             paste(paste(as.vector(objeto_0[[1]]$Total)),
+                             paste(paste(as.vector(objeto_0[[1]]$`Número de mortes`)),
                                    collapse = ' ')),']',sep = '')
 
-#Colocar o nome da coluna depois de "objeto_0[[1]]$"
 
 texto<-paste('{"title":{"text":"',titulo,
              '","subtext":"',subtexto,
              '","sublink":"',link,'"},',
-             '"tooltip":{"trigger":"axis"},',
+             '"tooltip":{"trigger":"item","responsive":true,"position":"top","formatter":"{b}: {c} mil"},',
              '"toolbox":{"left":"center","orient":"horizontal","itemSize":20,"top":20,"show":true,',
              '"feature":{"dataZoom":{"yAxisIndex":"none"},',
              '"dataView":{"readOnly":false},',
-             '"restore":{},"saveAsImage":{}}},"legend":{"show":true,"top":"bottom"},"xAxis":{"type":"category",',
+             '"restore":{},"saveAsImage":{}}},"legend":{"show":false,"top":"bottom"},"xAxis":{"type":"category",',
              '"data":',data_axis,'},',
-             '"yAxis":{"type":"value","axisLabel":{"formatter":"{value}"}},',
+             '"yAxis":{"type":"value","axisLabel":{"formatter":"{value} mil"}},',
+             '"graphic":[{"type":"text","left":"center","top":"bottom","z":100,"style":{"fill":"gray","text":"Obs: Ponto é separador decimal", "font":"8px sans-srif","fontSize":12}}],',
              '"series":[{"name":"',nomes[2],'","data":',data_serie,',',
-             '"type":"bar","color":"',corsec_recossa_azul[5],'","showBackground":true,',
-             '"backgroundStyle":{"color":"rgba(180, 180, 180, 0.2)"},"symbol":"',simbolo_linhas[4],
-             '","symbolSize":10,"itemStyle":{"borderRadius":10,"borderColor":"',corsec_recossa_azul[5],'","borderWidth":2}}',
+             '"type":"line","color":"',corsec_recossa_azul[1],'","showBackground":true,',
+             '"backgroundStyle":{"color":"rgba(180, 180, 180, 0.2)"},"symbol":"',simbolo_linhas[1],
+             '","symbolSize":10,"itemStyle":{"borderRadius":10,"borderColor":"',corsec_recossa_azul[1],'","borderWidth":2}}',
              ']}',sep='')
 
-#SAIDA_POVOAMENTO$CODIGO[i] <- texto  
+#SAIDA_POVOAMENTO$CODIGO[i] <- texto   
 texto<-noquote(texto)
 
 
@@ -152,3 +134,8 @@ objeto_autm <- SAIDA_POVOAMENTO %>% list()
 exportJson_aut <- toJSON(objeto_autm)
 
 #write(exportJson_aut,file = paste('data/povoamento.json'))
+
+
+
+
+
